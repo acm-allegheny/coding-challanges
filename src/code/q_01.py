6bebe4e32abcea53eb25dca02072dcc667b0efa6
benedekaibas <kaibas.benedek02@gmail.com>
2025-05-17 17:30:31 +0200

    added folders and file

---
+++ src/code/q_01.py
@@ -0,0 +1,10 @@
+"""First coding challange."""
+
+def first_problem():
+    """This is the first problem you have to complete."""
+    return 0
+
+if __name__ == "__main__":
+    result = first_problem()
+    print(result)
+
